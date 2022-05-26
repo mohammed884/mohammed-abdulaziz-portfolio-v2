20362f4425eb266e91c28cb592d5013a372927e1
Mohammed Abdulaziz <80047190+mohammed884@users.noreply.github.com>
2022-05-26 15:15:02 +0300

    Update actions.js

--- actions/actions.js
+++ actions/actions.js
@@ -2,12 +2,12 @@ import axios from "axios";
 const { NEXT_PUBLIC_API_URL } = process.env
 export const getProjects = async () => {
     console.log(NEXT_PUBLIC_API_URL);
-    const url = `${NEXT_PUBLIC_API_URL}/api/project`;
+    const url = `${NEXT_PUBLIC_API_URL}/project`;
     const { data } = await axios(url);
     return data
 }
 export const getProjectDetails = async title => {
-    const url = `${NEXT_PUBLIC_API_URL}/api/project/details`
+    const url = `${NEXT_PUBLIC_API_URL}/project/details`
     const { data } = await axios(url, { headers: { title } });
     return data
 }
@@ -18,20 +18,20 @@ export const getReviews = async () => {
 }
 export const getRole = async token => {
     if (token === "") false
-    const url = `${NEXT_PUBLIC_API_URL}/api/admin`
+    const url = `${NEXT_PUBLIC_API_URL}/admin`
     const { data } = await axios(url, { headers: { token } });
     return data.success
 };
 
 export const getAnsweredClients = async token => {
     if (token === "") return { success: false }
-    const url = `${NEXT_PUBLIC_API_URL}/api/clients/answered`
+    const url = `${NEXT_PUBLIC_API_URL}/clients/answered`
     const { data } = await axios(url, { headers: { token } });
     return data
 }
 export const getPotentialClients = async token => {
     if (token === "") return { success: false }
-    const url = `${NEXT_PUBLIC_API_URL}/api/clients/potential`
+    const url = `${NEXT_PUBLIC_API_URL}/clients/potential`
     const { data } = await axios(url, { headers: { token } });
     return data
 }
