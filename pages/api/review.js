023b35cec4bdc6a79e1dc4bf30aaff5046326cc0
Mohammed Abdulaziz <mohammed.knite25@gmail.com>
2022-05-27 22:45:39 +0300

    fix multer

--- pages/api/review.js
+++ pages/api/review.js
@@ -1,19 +1,19 @@
-// import Review from "../../models/review";
-// import multer from "multer";
+import Review from "../../models/review";
+import multer from "multer";
 import nc from "next-connect";
 import { nanoid } from 'nanoid';
-// import reviewSchema from "../../validation/review";
-// import dayjs from "dayjs";
-// import isAdmin from "../../middleware/isAdmin";
-// import database from "../../middleware/database";
-// import fs from "fs"
-// const upload = multer({
-//     limits: { fileSize: 5242880 },
-//     storage: multer.diskStorage({
-//         destination: './public/uploads',
-//         filename: (req, file, cb) => cb(null, `${nanoid()}-${file.originalname}`),
-//     }),
-// });
+import reviewSchema from "../../validation/review";
+import dayjs from "dayjs";
+import isAdmin from "../../middleware/isAdmin";
+import database from "../../middleware/database";
+import fs from "fs"
+const upload = multer({
+    limits: { fileSize: 5242880 },
+    storage: multer.diskStorage({
+        destination: '/app/public/uploads',
+        filename: (req, file, cb) => cb(null, `${nanoid()}-${file.originalname}`),
+    }),
+});
 const handler = nc({
     onError: (err, req, res, next) => {
         console.error(err.stack);
@@ -23,7 +23,7 @@ const handler = nc({
         res.status(404).end("Page is not found");
     },
 })
-// handler.use(database);
+handler.use(database);
 handler.get(async (req, res) => {
     try {
         //SEND ALL REVIEWS
@@ -35,53 +35,53 @@ handler.get(async (req, res) => {
         res.send("error")
     }
 })
-// handler.post(upload.single("cover"), async (req, res) => {
-//     try {
-//         //STRUCTURE AND VALIDATE DATA
-//         const { name, description, stars, projectLink } = req.body;
-//         if (req.file) {
-//             var fileName = req.file.filename;
-//             var mimetype = req.file.mimetype;
-//         }
-//         if (projectLink && projectLink.slice(0, 8) !== "https://") return res.send({ success: false, message: "اكتب رابط بصيغة صحيحة" })
-//         await reviewSchema.validateAsync({ name, description, stars, mimetype });
+handler.post(upload.single("cover"), async (req, res) => {
+    try {
+        //STRUCTURE AND VALIDATE DATA
+        const { name, description, stars, projectLink } = req.body;
+        if (req.file) {
+            var fileName = req.file.filename;
+            var mimetype = req.file.mimetype;
+        }
+        if (projectLink && projectLink.slice(0, 8) !== "https://") return res.send({ success: false, message: "اكتب رابط بصيغة صحيحة" })
+        await reviewSchema.validateAsync({ name, description, stars, mimetype });
 
-//         //CREATE THE REVIEW
-//         await new Review({
-//             name,
-//             description,
-//             stars,
-//             projectLink,
-//             cover: fileName,
-//             date: dayjs().format("MMM D, YYYY"),
-//             analysis_date: dayjs().format("M/YYYY"),
-//         }).save();
+        //CREATE THE REVIEW
+        await new Review({
+            name,
+            description,
+            stars,
+            projectLink,
+            cover: fileName,
+            date: dayjs().format("MMM D, YYYY"),
+            analysis_date: dayjs().format("M/YYYY"),
+        }).save();
         
-//         res.send({ success: true, message: "تم نشر تجربتك بنجاح" })
-//     } catch (err) {
-//         //REMOVE UPLOADED FILE
-//         if (fileName) {
-//             fs.unlinkSync(`./public/uploads/${fileName}`)
-//         }
-//         if (!err.isJoi) return res.send({ success: false, message: err.message })
-//         const message = err.details[0].message.replace(/"/g, "");
-//         res.send({ success: false, message })
+        res.send({ success: true, message: "تم نشر تجربتك بنجاح" })
+    } catch (err) {
+        //REMOVE UPLOADED FILE
+        if (fileName) {
+            fs.unlinkSync(`./public/uploads/${fileName}`)
+        }
+        if (!err.isJoi) return res.send({ success: false, message: err.message })
+        const message = err.details[0].message.replace(/"/g, "");
+        res.send({ success: false, message })
 
-//     }
-// });
-// handler.delete(isAdmin, async (req, res) => {
-//     try {
-//         const _id = req.headers._id;
-//         const review = await Review.findOne({ _id });
-//         if (review.cover !== "default.png") fs.unlinkSync(`./public/uploads/${review.cover}`);
-//         await review.delete();
+    }
+});
+handler.delete(isAdmin, async (req, res) => {
+    try {
+        const _id = req.headers._id;
+        const review = await Review.findOne({ _id });
+        if (review.cover !== "default.png") fs.unlinkSync(`./public/uploads/${review.cover}`);
+        await review.delete();
         
-//         res.send({ success: true })
-//     } catch (err) {
-//         console.log(err);
-//         res.send({ success: false, message: err.message })
-//     }
-// });
+        res.send({ success: true })
+    } catch (err) {
+        console.log(err);
+        res.send({ success: false, message: err.message })
+    }
+});
 export const config = {
     api: {
         bodyParser: false,
