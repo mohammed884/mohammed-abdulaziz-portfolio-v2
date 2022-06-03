1e79c042466df73bee68aecc95218b4973d2b61a
Mohammed Abdulaziz <mohammed.knite25@gmail.com>
2022-06-04 01:20:30 +0300

    fix redirecting in writing review page

--- pages/review.js
+++ pages/review.js
@@ -43,7 +43,7 @@ export default function Review() {
             setShowConfetti(true);
             setTimeout(() => {
                 setShowConfetti(false);
-                window.history.back()
+                Router.push("/")
             }, 2200)
         } else {
             setErrorMessage(data.message)
