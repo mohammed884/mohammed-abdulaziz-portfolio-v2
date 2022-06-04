d7b6b163d34f309806b5f8d735488faa4ade8691
Mohammed Abdulaziz <mohammed.knite25@gmail.com>
2022-06-04 17:00:21 +0300

    fix cloudinary configure

--- utilities/cloudinaryMethods.js
+++ utilities/cloudinaryMethods.js
@@ -1,16 +1,15 @@
 import { v2 as cloudinary } from "cloudinary";
 
-//CONFIGURE CLOUDINARY
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-});
 const single = async (image) => {
     try {
         //CHECK IF IMAGE ARE REQUIRED
         if (!image) return { success: false, message: "image is required" };
-
+        //CONFIGURE CLOUDINARY
+        cloudinary.config({
+            cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+            api_key: process.env.CLOUDINARY_API_KEY,
+            api_secret: process.env.CLOUDINARY_API_SECRET
+        });
         //UPLOAD TO CLOUDINARY
         const result = await cloudinary.uploader.upload(image.path, {
             upload_preset: "portfolio_uploads",
@@ -18,7 +17,7 @@ const single = async (image) => {
             allowed_formats: "jpg, png, jpeg"
         })
         //RETURN THE REQUIRED CREDENTIALS
-        return {url:result.secure_url,publicId:result.public_id};
+        return { url: result.secure_url, publicId: result.public_id };
     } catch (err) {
         console.log(err);
         return { success: false, message: err.message }
@@ -29,6 +28,12 @@ const multiple = async (images) => {
         //CHECK IF IMAGE ARE REQUIRED
         if (!images) return { success: false, message: "images are required" }
         const paths = [];
+        //CONFIGURE CLOUDINARY
+        cloudinary.config({
+            cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+            api_key: process.env.CLOUDINARY_API_KEY,
+            api_secret: process.env.CLOUDINARY_API_SECRET
+        });
         //LOOP OVER IMAGES AND UPLOAD IT
         for (let i = 0; i < images.length; i++) {
             const img = images[i];
@@ -49,9 +54,16 @@ const multiple = async (images) => {
 };
 const deleteUpload = async (publicId) => {
     try {
+        //CONFIGURE CLOUDINARY
+        cloudinary.config({
+            cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+            api_key: process.env.CLOUDINARY_API_KEY,
+            api_secret: process.env.CLOUDINARY_API_SECRET
+        });
+
         await cloudinary.uploader.destroy(publicId);
     } catch (err) {
         console.log(err);
     }
 }
-export const cloudinaryMethods = { multiple, single,deleteUpload };
+export const cloudinaryMethods = { multiple, single, deleteUpload };
