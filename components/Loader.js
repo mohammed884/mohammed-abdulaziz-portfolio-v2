7942b5d545bb56ff1ba9162ac37150171822e2c5
Mohammed Abdulaziz <mohammed.knite25@gmail.com>
2022-06-04 17:40:04 +0300

    fix loader class name

--- components/Loader.js
+++ components/Loader.js
@@ -4,7 +4,7 @@ export default function Loader() {
     return (
         <div className="w-[100%] h-[100%] absolute flex justify-center items-center z-20">
             <div className="w-[100%] h-[100%] bg-black opacity-50 absolute"></div>
-            <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+            <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
         </div>
     )
 }
