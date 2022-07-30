33b4f70685af0dfab70981fadcc55917bdcec79b
Mohammed Abdulaziz <mohammed.knite25@gmail.com>
2022-07-30 03:34:50 +0300

    fix Footer bugs

--- components/Footer.js
+++ components/Footer.js
@@ -7,13 +7,13 @@ export default function Footer() {
     <footer className="w-[100%] h-[10vh] flex flex-col justify-center items-center">
       <p>Developed By Mohammed Abdulaziz 2022 ©</p>
       <div className="mt-3">
-        <Link href="https://www.instagram.com/dev.mohammed1/?hl=en">
-          <span class="text-xl text-red-500 cursor-pointer">
+        <Link passHref href="https://www.instagram.com/dev.mohammed1/?hl=en">
+          <span className="text-xl text-red-500 cursor-pointer">
             <FontAwesomeIcon icon={faInstagram} />
           </span>
         </Link>
-        <Link _blank href="https://www.instagram.com/dev.mohammed1/?hl=en">
-          <span class="ml-4 text-xl text-blue_color cursor-pointer">
+        <Link passHref href="https://www.instagram.com/dev.mohammed1/?hl=en">
+          <span className="ml-4 text-xl text-blue_color cursor-pointer">
             <FontAwesomeIcon icon={faLinkedinIn} />
           </span>
         </Link>
